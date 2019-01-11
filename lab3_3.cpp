cb504e8f70ec026116498e6833bd904e66464fbc
spideypepo <spideypepo@spideypepos-MacBook-Pro.local>
2019-01-11 17:26:11 +0700

    ข้อ  3

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -3,7 +3,15 @@ using namespace std;
 
 int main()
 {
-    cout << "PEPO";
-    cout << "CPECMU";
-return 0;
+    int n=2;
+    double ans=0,n1=0;
+    while(n!=70)
+    {
+    n1=1.0/n;
+    ans=ans+n1;
+    n=n+1;
+    cout << n1 << endl;
+    }
+    cout << ans << n << endl;
+    return 0;
 }
