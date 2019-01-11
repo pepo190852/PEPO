139dfc8999db7678bbc56e03613f8258f295f05c
spideypepo <spideypepo@spideypepos-MacBook-Pro.local>
2019-01-11 17:31:18 +0700

    ข้อ4

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 int main()
 {
-    int n=2;
+    int n=6;
     double ans=0,n1=0;
-    while(n!=70)
+    while(n!=20)
     {
     n1=1.0/n;
     ans=ans+n1;
     n=n+1;
     cout << n1 << endl;
     }
-    cout << ans << n << endl;
+    cout << ans << endl;
     return 0;
 }
